4680a5980221a4b451cb4ec28c3bb79b0f3d1e45
Raz Aloni <Raz Aloni@DESKTOP-QSSAVPI>
2016-08-04 18:04:27 -0700

    First Test

--- Program.cs
+++ Program.cs
@@ -11,12 +11,29 @@ namespace TestingOutNetduino
 {
     public class Program
     {
+        private static bool toggleEnabled = false;
+
         public static void Main()
         {
-            // write your code here
+            // Init Ports
+            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+            var buttonPort = new InterruptPort(Pins.ONBOARD_BTN, true, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeHigh);
 
+            buttonPort.OnInterrupt += ButtonPort_OnInterrupt;
 
+            while (true)
+            {
+                if(toggleEnabled)
+                {
+                    ledPort.Write(!ledPort.Read());
+                    Thread.Sleep(500);
+                }
+            }
         }
 
+        private static void ButtonPort_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            toggleEnabled = !toggleEnabled;
+        }
     }
 }
